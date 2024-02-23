765e5279c2b85ea40deaca7d204aa9363fe0c63f
Ing. Danny Colmenares Pérez <dannycolmenarespdv@gmail.com>
2024-02-23 11:57:22 -0400

    Create calculadoraformatomiles.py
    Programa que muestra número con formato de separador de miles

---
+++ calculadoraformatomiles.py
@@ -0,0 +1,17 @@
+"""
+  Este es un programa que recibe dos valotes por teclado
+  suma los valores recibidos
+  muestra el resultado
+  en caso de ser el resultado mayor de 1.000
+      muestra el resultado con formato de separador de miles
+"""
+
+# Se solicitan los operandos para sumarlos
+x = int(input("Ingrese el valor de x: "))
+y = int(input("Ingrese el valor de y: "))
+
+# Se guarda el resultado en una variable
+z = x + y
+
+# Se muestra el resultado de la suma con formato de separador de miles
+print(f"El resultado es: {z:,}")
